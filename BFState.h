06c7a14496532e17afcfa1b4d70d397be4fb807f
Ethan <ethan@ubuntu.(none)>
2013-01-15 14:17:17 -0800

    Fixed compilation issues with structure

---
+++ BFState.h
@@ -0,0 +1,5 @@
+typedef struct BFState{
+	//The Turing Tape in this tarpit that we'll be playing with.
+	char  tape[30000];
+
+} BFState;
--- test.c
+++ test.c
@@ -1,19 +1,27 @@
 #include "IOHelp.h" //Also includes string,io,stdlib
-
-//The Turing Tape in this tarpit that we'll be playing with.
-char tape[30000];
+#include "BFState.h"//All the BF compiler stuffs
 
 //Use proper form of main to accept cmdline
 int main(int argc, char *argv[]){
 	//Look at the command line arguments
 	FILE * fp = parseArguments(argc,argv);
 	
-	if(fp == NULL){
+	
 
+	if(fp == NULL){
+		//No file but are there brainfuck commands being passed to the program?
+		if(argc > 1){
+			//Yes there are! Do stuff with them!
+		}else{
+			//No arguments. Begin interactive interpreter here or do nothing
+		}
 	}
+
+	struct BFState thing;
+
+	printf("%s\n",(char*)thing.tape );
 
 	printf("\nCompiled\n\n");
 	
-	getchar();
 	return(0);
 }
