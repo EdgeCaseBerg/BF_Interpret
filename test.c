828627f4d711b3b2140362e5660011c90067b485
EJEHardenBerg <ejayeldridge@gmail.com>
2013-01-17 18:26:11 -0500

    The program now will take BF statements as arguments and display the results. (tested with hellow world string pasted to cmd line

--- test.c
+++ test.c
@@ -12,34 +12,17 @@ int main(int argc, char *argv[]){
 		//No file but are there brainfuck commands being passed to the program?
 		if(argc > 1){
 			//Yes there are! Do stuff with them!
+			int i=1;
+			for(; i < argc; i++){
+				translateBF(&thing,argv[i]);
+			}
 		}else{
 			//No arguments. Begin interactive interpreter here or do nothing
 		}
 	}
 
-	//Tests
-	/*
-	incrementDataPointer(&thing);
-	incrementCurrentByte(&thing);
-	decrementDataPointer(&thing);
-	decrementDataPointer(&thing);
-	
-	printf("%i\n",thing.tape[thing.dataPointer]);
-
-	int i;
-	for(i=0; i < 65; i++){
-		incrementCurrentByte(&thing);	
-	}
-	
-
-	outputByte(&thing);
-	inputByte(&thing);
-
-	printf("%c\n",thing.tape[thing.dataPointer]);
-	printf("%i\n",thing.dataPointer );
-	printf("%i\n",thing.currentSize );
-	*/
-	translateBF(&thing,"++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.");
+	//Tests by doing Hellow World.
+	//translateBF(&thing,"++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.");
 
 	printf("\nCompiled\n\n");
 
