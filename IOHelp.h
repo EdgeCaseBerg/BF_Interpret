527d35b40aea4e64492b7939b854ad98eb9c745c
EJEHardenBerg <ejayeldridge@gmail.com>
2013-01-17 23:34:00 -0500

    wrote function to read and return  the strings from a file

--- IOHelp.h
+++ IOHelp.h
@@ -17,6 +17,7 @@
 #ifndef __IOHELP_H_INCLUDED__
 	#define __IOHELP_H_INCLUDED__
 
+//Opens a file whose name is passed through cmdline arguments if it exists
 FILE * parseArguments(int argc, char * argv[]){
 	if(argc > 1){
 		//Look for -f flag for file input
@@ -38,6 +39,53 @@ FILE * parseArguments(int argc, char * argv[]){
 		}
 	}
 	return NULL;
+}
+
+//Essentially load a string into a char array while checking the validatity of the 
+//string itself. (braces closed)
+char * readFromFile(FILE * fp){
+	int temp;
+	//Allocating 30,000 which is the standard brainfuck tape size.
+	int size = 30000;
+	char * building = (char *) malloc(size);
+	int braceCheck = 0;
+	int s =0;
+	while((temp = fgetc(fp)) != EOF){
+		if(s > size-1){
+			//If s is 29,999 then we need to make more room for the null character and such.
+			//So double the size of the array and copy the old contents over.
+			char * tempArray = (char *)malloc(size*2);
+			int t;
+			for(t=0; t < size; t++){
+				tempArray[t] = building[t];
+			}
+			size = size*2;
+			building = tempArray;
+		}
+		if(temp == '['){
+			braceCheck++;
+		}else if(temp == ']'){
+			braceCheck--;
+		}
+		building[s] = (char)temp;
+		s++;
+	}
+	//End it with a null character
+	if(s < size-1){
+		building[s] = '\0';	
+	}else{
+		//Seriously? One off.
+		char * tempArray = (char *)malloc(size + 1);
+		//Reuse s
+		s=0;
+		for(; s < size+1; s++){
+			tempArray[s] = building[s];
+		}
+		tempArray[size-1] = '\0';
+		building = tempArray;
+	}
+	
+	return building;
 }
 
 #endif
